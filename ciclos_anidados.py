1ed8e8b81d980f6462362e84ee985d56699e7a6a
BraSteAU <brayan_8924@hotmail.com>
2024-12-06 19:09:28 -0500

    primera parte
    ejemplos para recorrer matrices

--- ciclos_anidados.py
+++ ciclos_anidados.py
@@ -5,18 +5,32 @@
 #         print(f"i={i}, j={j}")
 
 
-#procesamiento de matrices con cilcos anidados
+#procesamiento de matrices con ciclos anidados
 
 #Ejemplo 1: Iprimir una matriz de forma que quede representada con su representacion matematica
 
-matriz=[
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
+# matriz=[
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+# suma=0
+# for fila  in matriz:
+#     for elemento in fila:
+#         print(elemento,end=" ")
+#         suma+=elemento
+#     print()
+# print(f"Suma total: {suma}")    
 
-for fila  in matriz:
-    for elemento in fila:
-        print(elemento,end=" ")
-    print()
 
+# for fila in matriz:
+#     for elemento in fila:
+#         suma+=elemento
+# print(f"suma total: {suma}")
+
+# contador_pares=0
+# for fila in matriz:
+#     for elemento in fila:
+#         if elemento%2==0:
+#             contador_pares+=1
+# print(f"Hay {contador_pares} numeros pares en la matriz")            
